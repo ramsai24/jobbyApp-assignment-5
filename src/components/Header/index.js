815a8400d40a27309b357debe3a1d42a9bd5cee1
jnrsai24 <jnrsai24@gmail.com>
2023-07-31 14:40:21 +0000

    replace error in console and passing imporper props in function component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,7 @@
 import {Link, withRouter} from 'react-router-dom'
+import {AiFillHome} from 'react-icons/ai'
+import {BsFillBriefcaseFill} from 'react-icons/bs'
+import {FiLogOut} from 'react-icons/fi'
 import Cookies from 'js-cookie'
 import './index.css'
 
@@ -19,18 +22,46 @@ const Header = props => {
             alt="website logo"
           />
         </Link>
-        <ul className="nav-items">
+        <ul className="nav-items-container">
           <li>
-            <Link to="/">Home</Link>
+            <Link className="nav-link-item" to="/">
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/Jobs">Jobs</Link>
+            <Link className="nav-link-item" to="/Jobs">
+              Jobs
+            </Link>
           </li>
         </ul>
-        <button type="button" onClick={onLogOut}>
+        <button className="logoutBtn" type="button" onClick={onLogOut}>
           Logout
         </button>
       </nav>
+      <div className="nav-mobi-container">
+        {/* <div> */}
+        <Link to="/">
+          <img
+            className="website-logo"
+            src="https://assets.ccbp.in/frontend/react-js/logo-img.png"
+            alt="website logo"
+          />
+        </Link>
+        {/* </div> */}
+        <ul className="nav-items-container">
+          <li>
+            <Link className="nav-link-item" to="/">
+              <AiFillHome className="icons" />
+            </Link>
+          </li>
+          <li>
+            <Link className="nav-link-item" to="/Jobs">
+              <BsFillBriefcaseFill className="icons" />
+            </Link>
+          </li>
+          <FiLogOut className="nav-link-item icons" onClick={onLogOut} />
+        </ul>
+      </div>
     </div>
   )
 }
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,28 @@
 import Header from '../Header'
 import './index.css'
 
-const Home = () => (
-  <div>
-    <Header />
-    <div>
-      <h1>Find The Job That Fits Your Life</h1>
-      <p>
-        Million of people are searching for jobs,salary information company
-        reviews. Find the job that fits your abilities and potential.
-      </p>
+const Home = () => {
+  const redirectToJobsRoute = props => {
+    const {history} = props
+    console.log(history)
+    history.replace('/Jobs')
+  }
+
+  return (
+    <div className="home-bg-container">
+      <Header />
+      <div className="home-content-container">
+        <h1 className="home-title">Find The Job That Fits Your Life</h1>
+        <p>
+          Million of people are searching for jobs,salary information company
+          reviews. Find the job that fits your abilities and potential.
+        </p>
+      </div>
+      <button type="button" onClick={redirectToJobsRoute}>
+        Find Jobs
+      </button>
     </div>
-  </div>
-)
+  )
+}
 
 export default Home
