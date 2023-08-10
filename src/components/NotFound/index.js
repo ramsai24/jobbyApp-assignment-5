86c1498f75733e0d42d715fcde10bd4be8385e06
jnrsai24 <jnrsai24@gmail.com>
2023-08-10 01:58:35 +0000

    CSS is pending and All test cases passed

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,7 +1,7 @@
 import './index.css'
 
 const NotFound = () => (
-  <div>
+  <div className="not-found-container">
     <img
       src="https://assets.ccbp.in/frontend/react-js/jobby-app-not-found-img.png"
       alt="not found"
